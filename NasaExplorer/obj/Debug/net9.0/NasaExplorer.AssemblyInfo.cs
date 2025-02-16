26f3dca3b955cfd4215404fda06a66c3f5868251
zaferemre <emrecalisir34@gmail.com>
2025-02-16 16:21:54 +0100

    Railway Setup

--- NasaExplorer/obj/Debug/net9.0/NasaExplorer.AssemblyInfo.cs
+++ NasaExplorer/obj/Debug/net9.0/NasaExplorer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NasaExplorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93c76c75703088d339edfeb745063634e3256d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NasaExplorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NasaExplorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
